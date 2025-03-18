8782d3233709b2f111ca91a0f8c9c7ae5999ff0e
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2025-03-19 00:22:49 +0530

    feat: add step 3 of assessment, ie, adding \n as delimiter

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function add(numbers) {
     if (!numbers) return 0;
-    return numbers?.split(',')?.reduce((acc, curr) => acc + +curr, 0);
+    return String(numbers)?.split(/[,\n]/)?.reduce((acc, curr) => acc + +curr, 0);
 }
+
+console.log(add('7\n6,3,4'));
 
 module.exports = { add };
--- test/server.test.js
+++ test/server.test.js
@@ -13,4 +13,20 @@ describe('Test Add Function', () => {
         let result = add('1,5');
         expect(result).toBe(6);
     })
+    test('should return 94136 for an input of "54,89,5325,88668"', () => {
+        let result = add('54,89,5325,88668');
+        expect(result).toBe(94136);
+    })
+    test('should return 72222221 for an input of "5555555,66666666"', () => {
+        let result = add('5555555,66666666');
+        expect(result).toBe(72222221);
+    })
+    test('should return -1 for an input of "-5,5,3,-4"', () => {
+        let result = add('-5,5,3,-4');
+        expect(result).toBe(-1);
+    })
+    test('should return 20 for an input of "7\n6,3,4"', () => {
+        let result = add('7\n6,3,4');
+        expect(result).toBe(20);
+    })
 });
