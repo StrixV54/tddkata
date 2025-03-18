99e41468f91a5864b00b518df9cf0cfe964269d3
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2025-03-18 23:41:30 +0530

    feat: add step 1 of assessment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+function add(numbers) {
+    if (!numbers) return 0;
+    return numbers?.split(',')?.reduce((acc, curr) => acc + +curr, 0);
+}
+
+module.exports = { add };
--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,16 @@
-describe('Test Server is running', () => {
-    test('check "Hello, World!" is printed in console', () => {
-        const consoleLog = jest.spyOn(console, 'log');
-        console.log('Hello, World!');
-        expect(consoleLog).toHaveBeenCalledWith('Hello, World!');
+const { add } = require('..');
+
+describe('Test Add Function', () => {
+    test('should return 0 for an input of "" (empty string)', () => {
+        let result = add('');
+        expect(result).toBe(0);
     });
+    test('should return 1 for an input of "1"', () => {
+        let result = add('1');
+        expect(result).toBe(1);
+    })
+    test('should return 6 for an input of "1,5"', () => {
+        let result = add('1,5');
+        expect(result).toBe(6);
+    })
 });
